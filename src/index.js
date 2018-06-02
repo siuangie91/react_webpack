629d256bd886b1b44ce375aca4f8cbc508d7d275
siuangie91 <siuangie91@gmail.com>
2018-06-02 18:24:21 -0400

    boilerplate for with_sass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './scss/app.scss';
+
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
@@ -9,6 +11,7 @@ class App extends React.Component {
 			<Fragment>
 				<h1>Hello world! New copy</h1>
 				<Paragraph />
+				<p>More copy! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, sequi quasi cum similique magni, nisi nulla repudiandae iste, ullam ipsum labore rerum non eligendi facilis, velit obcaecati qui laudantium commodi.</p>
 			</Fragment>
 
 		); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -8,17 +9,28 @@ module.exports = {
 		filename: 'bundle.js'
 	},
 	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			use: {
-				loader: 'babel-loader'
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.scss$/,
+				// use style-loader in place of MiniCssExtractPlugin if you want to insert css via <style> tag instead of having a separate file
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
 			}
-		}]
+		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html'
-		})
+		}),
+		new MiniCssExtractPlugin({
+        // both options are optional
+        filename: "app.css",
+    })
 	]
 }
